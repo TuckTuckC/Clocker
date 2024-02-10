02f04221c17e5592eafeae1439844687874e8e9e
Tucker Craig <tuckermcraig@gmail.com>
2024-02-10 18:15:15 -0500

    adds navigation bar with functioning navigation;

---
+++ lib/pages/timeclock.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class TimeClock extends StatelessWidget {
+  const TimeClock({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Time Clock"))
+    );
+  }
+}
